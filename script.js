8453fbc5c5596614cc20c9a1ac7672d5d5087059
Haris Kovac <hariskovac19@gmail.com>
2023-04-01 16:21:36 -0500

    Create displayController module

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ const gameBoard = (() => {
 
   return { getBoardArray };
 })();
+
+const displayController = (() => {
+  let ties = 0;
+
+  const updateBoardDisplay = () => {
+    let index = 0;
+    const array = gameBoard.getBoardArray();
+    const spaces = document.querySelectorAll('.space');
+    spaces.forEach(space => {
+      space.textContent = array[index];
+      index += 1;
+    });
+  };
+
+  return { updateBoardDisplay };
+})();
