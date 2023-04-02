073abb1bb5d37da305b0284e8ce435b01df1c3e5
Haris Kovac <hariskovac19@gmail.com>
2023-04-02 13:42:20 -0500

    Add checkWinner method to gameController module

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ const displayController = (() => {
       gameBoard.updateBoardArray(square.dataset.index, currentMarker);
       updateBoardDisplay();
       changeTurn();
+      checkWinner();
     });
   });
 
@@ -40,9 +41,38 @@ const displayController = (() => {
 
   const changeTurn = () => {
     currentMarker = currentMarker === xMarker ? oMarker : xMarker;
-  }
+  };
 
-  return { updateBoardDisplay, changeTurn };
+  const checkWinner = () => {
+    const array = gameBoard.getBoardArray();
+    const winningMoves = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    // Checks for a winning combination of plays
+    for (let i = 0; i < winningMoves.length; i++) {
+      if (
+        array[winningMoves[i][0]] !== ''
+        && array[winningMoves[i][0]] === array[winningMoves[i][1]] 
+        && array[winningMoves[i][1]] === array[winningMoves[i][2]]
+      ) {
+        console.log('Winner!');
+      }
+    } 
+
+    if (!array.includes('')) {
+      console.log('Tie Game');
+    }
+  };
+
+  return { updateBoardDisplay, changeTurn, checkWinner };
 })();
 
 const Player = (name) => {
