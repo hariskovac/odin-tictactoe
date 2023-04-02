fa3ca21143959a24afd81e2b5d72ffa0b60c61f1
Haris Kovac <hariskovac19@gmail.com>
2023-04-02 12:34:22 -0500

    Update boardArray/Display when a space is clicked

--- script.js
+++ script.js
@@ -8,18 +8,31 @@ const gameBoard = (() => {
     }
   };
 
-  return { getBoardArray };
+  return { getBoardArray, updateBoardArray };
 })();
 
 const displayController = (() => {
   let ties = 0;
+  let currentMarker;
+  const xMarker = 'media/icon-cross.svg';
+  const oMarker = 'media/icon-circle.svg';
+  const square = document.querySelectorAll('.board-square');
+  const spaces = document.querySelectorAll('.space');
 
+  // Updates boardArray and the board display when a square is clicked
+  square.forEach(function(square) {
+    square.addEventListener('click', function() {
+      gameBoard.updateBoardArray(square.dataset.index, currentMarker);
+      updateBoardDisplay();
+    });
+  });
+
+  // Updates the board display based on the values in the boardArray
   const updateBoardDisplay = () => {
     let index = 0;
     const array = gameBoard.getBoardArray();
-    const spaces = document.querySelectorAll('.space');
     spaces.forEach(space => {
-      space.textContent = array[index];
+      space.src = array[index];
       index += 1;
     });
   };
