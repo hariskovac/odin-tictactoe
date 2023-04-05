ee596a4567c9f495246920374f012b726365c01a
Haris Kovac <hariskovac19@gmail.com>
2023-04-05 18:54:01 -0500

    Fix updateScore method

--- script.js
+++ script.js
@@ -41,9 +41,9 @@ const gameBoard = (() => {
 const gameController = (() => {
   const playerx = Player('media/icon-cross.svg');
   const playero = Player('media/icon-circle.svg');
-  const xScore = document.querySelector('.x-score');
-  const oScore = document.querySelector('.o-score');
-  const tieScore = document.querySelector('.tie-score');
+  const xScoreDisplay = document.querySelector('.x-score');
+  const oScoreDisplay = document.querySelector('.o-score');
+  const tieScoreDisplay = document.querySelector('.tie-score');
   const squares = document.querySelectorAll('.board-square');
   const spaces = document.querySelectorAll('.space');
   const restartButton = document.querySelector('.restart-btn');
@@ -57,10 +57,9 @@ const gameController = (() => {
         gameBoard.updateBoardArray(square.dataset.row, square.dataset.col, playerx.marker);
         updateBoardDisplay();
         checkWinner(gameBoard.getBoardArray());
-        
+        updateScore();
         square.classList.toggle('full');
         aiLogic.findBestMove(gameBoard.getBoardArray());
-        updateScore();
       }
     });
   });
@@ -76,10 +75,13 @@ const gameController = (() => {
   // Checks for a winner or tie game
   const checkWinner = (board) => {
     for (let r = 0; r < 3; r++) {
+      winner = '';
       if (board[r][0] === board[r][1] && board[r][1] === board[r][2]) {
           if (board[r][0] === playerx.marker) {
+            winner = 'X';
             return -10;
           } else if (board[r][0] === playero.marker) {
+            winner = 'O';
             return 10;
           }
         }
@@ -88,8 +90,10 @@ const gameController = (() => {
     for (let c = 0; c < 3; c++) {
       if (board[0][c] === board[1][c] && board[1][c] === board[2][c]) {
           if (board[0][c] === playerx.marker) {
+            winner = 'X';
             return -10;
           } else if (board[0][c] === playero.marker) {
+            winner = 'O';
             return 10;
           }
         }
@@ -97,16 +101,20 @@ const gameController = (() => {
 
     if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
       if (board[1][1] === playerx.marker) {
+        winner = 'X';
         return -10;
       } else if (board[1][1] === playero.marker) {
+        winner = 'O';
         return 10;
       }
     }
 
     if (board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
       if (board[1][1] === playerx.marker) {
+        winner = 'X';
         return -10;
       } else if (board[1][1] === playero.marker) {
+        winner = 'O';
         return 10;
       }
     }
@@ -121,27 +129,29 @@ const gameController = (() => {
     }
 
     if (openSpaces === 0) {
+      winner = 'tie';
       return 0;
     } 
-    
   }
 
+  // Updates the score display
   const updateScore = () => {
-    if (winner === 10) {
+    if (winner === 'X') {
       playerx.addPoint();
     } 
-    if (winner === -10) {
+    if (winner === 'O') {
       playero.addPoint();
     }
-    if (winner === 0) {
+    if (winner === 'tie') {
       ties += 1;
     }
-    xScore.textContent = playerx.getScore();
-    oScore.textContent = playero.getScore();
-    tieScore.textContent = ties;
+    xScoreDisplay.textContent = playerx.getScore();
+    oScoreDisplay.textContent = playero.getScore();
+    tieScoreDisplay.textContent = ties;
     winner = '';
   }
 
+  // Resets the board
   const resetBoard = () => {
     spaces.forEach(space => {
       space.src = '';
@@ -154,7 +164,7 @@ const gameController = (() => {
 
   restartButton.addEventListener('click', resetBoard);
 
-  return { updateBoardDisplay, checkWinner };
+  return { updateBoardDisplay, checkWinner, updateScore };
 })();
 
 const aiLogic = (() => {
@@ -224,6 +234,8 @@ const aiLogic = (() => {
     // Plays the AI's move by updating the array and the display
     gameBoard.updateBoardArray(bestMove.row, bestMove.col, aiPlayer);
     gameController.updateBoardDisplay();
+    gameController.checkWinner(gameBoard.getBoardArray());
+    gameController.updateScore();
   }
 
   return { findBestMove };
